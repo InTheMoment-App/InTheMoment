ad431d70643ab00cedc4326d3c9d8c6c39bfda5b
Rhys Young <rhysyoung16@gmail.com>
2021-04-17 23:45:18 -0400

    Fixed up map file and added linter

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -9,13 +9,13 @@ import HomesScreen from '../screens/HomesScreen';
 import ChatsScreen from '../screens/ChatsScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 import {
-  BottomTabParamList, HomesParamList, ChatsParamList, ProfileParamList,
+    BottomTabParamList, HomesParamList, ChatsParamList, ProfileParamList,
 } from '../types';
 
 // You can explore the built-in icon families and icons on the web at:
 // https://icons.expo.fyi/
 function TabBarIcon(props: { name: React.ComponentProps<typeof Ionicons>['name']; color: string }) {
-  return <Ionicons size={25} style={{ marginBottom: -3 }} {...props} />;
+    return <Ionicons size={25} style={{ marginBottom: -3 }} {...props} />;
 }
 
 // Each tab has its own navigation stack, you can read more about this pattern here:
@@ -23,76 +23,76 @@ function TabBarIcon(props: { name: React.ComponentProps<typeof Ionicons>['name']
 const HomesStack = createStackNavigator<HomesParamList>();
 
 function HomesNavigator() {
-  return (
-    <HomesStack.Navigator>
-      <HomesStack.Screen
-        name="HomesScreen"
-        component={HomesScreen}
-        options={{ headerTitle: 'Find a home' }}
-      />
-    </HomesStack.Navigator>
-  );
+    return (
+        <HomesStack.Navigator>
+            <HomesStack.Screen
+                name="HomesScreen"
+                component={HomesScreen}
+                options={{ headerTitle: 'Find a home' }}
+            />
+        </HomesStack.Navigator>
+    );
 }
 
 const ChatsStack = createStackNavigator<ChatsParamList>();
 
 function ChatsNavigator() {
-  return (
-    <ChatsStack.Navigator>
-      <ChatsStack.Screen
-        name="ChatsScreen"
-        component={ChatsScreen}
-        options={{ headerTitle: 'Chats' }}
-      />
-    </ChatsStack.Navigator>
-  );
+    return (
+        <ChatsStack.Navigator>
+            <ChatsStack.Screen
+                name="ChatsScreen"
+                component={ChatsScreen}
+                options={{ headerTitle: 'Chats' }}
+            />
+        </ChatsStack.Navigator>
+    );
 }
 
 const ProfileStack = createStackNavigator<ProfileParamList>();
 
 function ProfileNavigator() {
-  return (
-    <ProfileStack.Navigator>
-      <ProfileStack.Screen
-        name="ProfileScreen"
-        component={ProfileScreen}
-        options={{ headerTitle: 'Profile' }}
-      />
-    </ProfileStack.Navigator>
-  );
+    return (
+        <ProfileStack.Navigator>
+            <ProfileStack.Screen
+                name="ProfileScreen"
+                component={ProfileScreen}
+                options={{ headerTitle: 'Profile' }}
+            />
+        </ProfileStack.Navigator>
+    );
 }
 
 const BottomTab = createBottomTabNavigator<BottomTabParamList>();
 
 export default function BottomTabNavigator() {
-  const colorScheme = useColorScheme();
+    const colorScheme = useColorScheme();
 
-  return (
-    <BottomTab.Navigator
-      initialRouteName="Homes"
-      tabBarOptions={{ activeTintColor: Colors[colorScheme].tint, showLabel: false }}
-    >
-      <BottomTab.Screen
-        name="Homes"
-        component={HomesNavigator}
-        options={{
-          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
-        }}
-      />
-      <BottomTab.Screen
-        name="Chats"
-        component={ChatsNavigator}
-        options={{
-          tabBarIcon: ({ color }) => <TabBarIcon name="chatbubbles" color={color} />,
-        }}
-      />
-      <BottomTab.Screen
-        name="Profile"
-        component={ProfileNavigator}
-        options={{
-          tabBarIcon: ({ color }) => <TabBarIcon name="person-circle" color={color} />,
-        }}
-      />
-    </BottomTab.Navigator>
-  );
+    return (
+        <BottomTab.Navigator
+            initialRouteName="Homes"
+            tabBarOptions={{ activeTintColor: Colors[colorScheme].tint, showLabel: false }}
+        >
+            <BottomTab.Screen
+                name="Homes"
+                component={HomesNavigator}
+                options={{
+                    tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
+                }}
+            />
+            <BottomTab.Screen
+                name="Chats"
+                component={ChatsNavigator}
+                options={{
+                    tabBarIcon: ({ color }) => <TabBarIcon name="chatbubbles" color={color} />,
+                }}
+            />
+            <BottomTab.Screen
+                name="Profile"
+                component={ProfileNavigator}
+                options={{
+                    tabBarIcon: ({ color }) => <TabBarIcon name="person-circle" color={color} />,
+                }}
+            />
+        </BottomTab.Navigator>
+    );
 }
