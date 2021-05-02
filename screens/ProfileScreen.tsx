b12639b799e6db9edb76d8e7bc750f906004b684
Rhys Young <rhysyoung16@gmail.com>
2021-05-02 16:00:22 -0400

    fixing linter issues

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import { StyleSheet, ScrollView } from 'react-native';
+import {
+    List, Avatar, Title, Paragraph,
+} from 'react-native-paper';
 import Layout from '../constants/Layout';
-import { List, Avatar, Title, Paragraph } from 'react-native-paper';
-import { Ionicons } from '@expo/vector-icons';
 
 // TODO: actually use code, commented out so it passes linter since we have no
 // real use for it currently
 // import { useQuery, gql } from '@apollo/client';
-import { Text, View } from '../components/Themed';
+import { View } from '../components/Themed';
 
 const styles = StyleSheet.create({
     container: {
@@ -56,8 +57,6 @@ export default function ProfileScreen() {
 
     // testQuery();
 
-
-
     return (
         <View style={styles.container}>
             <ScrollView>
@@ -71,22 +70,22 @@ export default function ProfileScreen() {
                     <List.Item
                         title="General"
                         description="Some general settings go here"
-                        left={props => <List.Icon {...props} icon="cog" />}
+                        left={(props) => <List.Icon {...props} icon="cog" />}
                     />
                     <List.Item
                         title="Appearance"
                         description="Customize the feel of the app"
-                        left={props => <List.Icon {...props} icon="brush" />}
+                        left={(props) => <List.Icon {...props} icon="brush" />}
                     />
                     <List.Item
                         title="Notifications"
                         description="Keep track of what is going on"
-                        left={props => <List.Icon {...props} icon="bell" />}
+                        left={(props) => <List.Icon {...props} icon="bell" />}
                     />
                     <List.Item
                         title="Security"
                         description="Change password etc"
-                        left={props => <List.Icon {...props} icon="security" />}
+                        left={(props) => <List.Icon {...props} icon="security" />}
                     />
                 </List.Section>
                 <List.Section>
@@ -94,14 +93,14 @@ export default function ProfileScreen() {
                     <List.Item
                         title="Logout"
                         description="Logout of your account."
-                        left={props => <List.Icon {...props} icon="logout" />}
+                        left={(props) => <List.Icon {...props} icon="logout" />}
                     />
                     <List.Item
                         title="Delete Acount"
                         description="Permanently delete your account"
-                        left={props => <List.Icon {...props} icon="delete" />}
+                        left={(props) => <List.Icon {...props} icon="delete" />}
                     />
-                </List.Section>                
+                </List.Section>
             </ScrollView>
         </View>
     );
