6a3c0bd1cee9ad1376c2ce3fa444882416d1fd91
Rhys Young <rhysyoung16@gmail.com>
2021-05-07 00:15:55 -0400

    fixing bug with imports

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,23 @@
-module.exports = function (api) {
+module.exports = api => {
   api.cache(true);
-  return {
+  const config ={
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            assets: './assets',
+            components: './components',
+            constants: './constants',
+            hooks: './hooks',
+            translations: './translations',
+            types: './types',
+            screens: './screens'
+          }
+        }
+      ]
+    ]
   };
+  return config;
 };
