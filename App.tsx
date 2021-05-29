895855563b8451fb08e76cbe74208c5f43f01a2c
Rhys Young <rhysyoung16@gmail.com>
2021-05-29 10:49:51 -0400

    Fixing linter errors

--- App.tsx
+++ App.tsx
@@ -2,7 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+// eslint doesn't like that there is no package referencing this
+// however this is how apollo expects it
+/* eslint-disable import/no-extraneous-dependencies */
 import { ApolloProvider } from '@apollo/client/react';
+/* eslint-enable import/no-extraneous-dependencies */
+
 import { Provider as PaperProvider } from 'react-native-paper';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
